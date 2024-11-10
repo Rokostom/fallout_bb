a390f357111ba8ed78d3ff676d939b7bbdf9b63d
rokostom <togglermap@gmail.com>
2024-11-10 18:21:42 +0700

    remove very important console log

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import './App.css';
 import router from './router';
 
 function App() {
-  console.log(process.env);
-
   return (
     <div className="App">
       <RouterProvider router={router} />
